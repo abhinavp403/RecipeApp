5162171f2c65cb21f14810bfa61ceeb6bad15b54
Abhinav <abhinavp403@gmail.com>
2021-03-20 20:19:42 -0500

    Ninth Commit - Added MVI and Pagination

---
+++ app/src/main/java/com/dev/abhinav/recipeapp/presentation/ui/recipelist/RecipeListEvent.kt
@@ -0,0 +1,8 @@
+package com.dev.abhinav.recipeapp.presentation.ui.recipelist
+
+sealed class RecipeListEvent {
+
+    object NewSearchEvent : RecipeListEvent()
+
+    object NextPageEvent : RecipeListEvent()
+}
--- app/src/main/java/com/dev/abhinav/recipeapp/presentation/ui/recipelist/RecipeListViewModel.kt
+++ app/src/main/java/com/dev/abhinav/recipeapp/presentation/ui/recipelist/RecipeListViewModel.kt
@@ -1,5 +1,6 @@
 package com.dev.abhinav.recipeapp.presentation.ui.recipelist
 
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.hilt.lifecycle.ViewModelInject
@@ -11,8 +12,9 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Named
 
-class RecipeListViewModel
+const val PAGE_SIZE = 30
 
+class RecipeListViewModel
 @ViewModelInject
 constructor(
     private val repository: RecipeRepository,
@@ -29,28 +31,86 @@ constructor(
 
     val loading = mutableStateOf(false)
 
+    val page = mutableStateOf(1)
+
+    private var recipeListScrollPosition = 0
+
     init {
-        newSearch()
+        onTriggerEvent(RecipeListEvent.NewSearchEvent)
     }
 
-    fun newSearch() {
+    fun onTriggerEvent(event: RecipeListEvent){
         viewModelScope.launch {
+            try {
+                when(event){
+                    is RecipeListEvent.NewSearchEvent -> {
+                        newSearch()
+                    }
+                    is RecipeListEvent.NextPageEvent -> {
+                        nextPage()
+                    }
+                }
+            }catch (e: Exception){
+                Log.e("TAG", "launchJob: Exception: ${e}, ${e.cause}")
+                e.printStackTrace()
+            }
+            finally {
+                Log.d("TAG", "launchJob: finally called.")
+            }
+        }
+    }
+
+    private suspend fun newSearch() {
+        loading.value = true
+        resetSearchState()
+        delay(2000)
+        val result = repository.search(
+            token = token,
+            page = 1,
+            query = query.value
+        )
+        recipes.value = result
+        loading.value = false
+    }
+
+    private suspend fun nextPage() {
+        if((recipeListScrollPosition + 1) >= (page.value * PAGE_SIZE)) {
             loading.value = true
-            resetSearchState()
-            delay(2000)
-            val result = repository.search(
-                token = token,
-                page = 1,
-                query = query.value
-            )
-            recipes.value = result
+            incrementPage()
+            delay(1000)
+            if(page.value > 1) {
+                val result = repository.search(
+                        token = token,
+                        page = page.value,
+                        query = query.value
+                )
+                appendRecipes(result)
+            }
             loading.value = false
         }
     }
 
+    private fun appendRecipes(recipes: List<Recipe>) {
+        val current = ArrayList(this.recipes.value)
+        current.addAll(recipes)
+        this.recipes.value = current
+    }
+
+    private fun incrementPage() {
+        page.value = page.value + 1
+    }
+
+    fun onChangeRecipeScrollPosition(position: Int) {
+        recipeListScrollPosition = position
+    }
+
     private fun resetSearchState(){
         recipes.value = listOf()
-        if(selectedCategory.value?.value != query.value) clearSelectedCategory()
+        page.value = 1
+        onChangeRecipeScrollPosition(0)
+        if(selectedCategory.value?.value != query.value) {
+            clearSelectedCategory()
+        }
     }
 
     private fun clearSelectedCategory(){
